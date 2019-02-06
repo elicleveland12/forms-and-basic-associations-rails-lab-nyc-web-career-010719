2ac1bc1c1d03360ece675ac119ae99852614c037
Elijah Cleveland <elicleveland12@gmail.com>
2019-02-06 17:13:09 -0500

    Done.

--- db/migrate/20160114013222_add_artist_id_and_genre_id_to_songs.rb
+++ db/migrate/20160114013222_add_artist_id_and_genre_id_to_songs.rb
@@ -2,5 +2,5 @@ class AddArtistIdAndGenreIdToSongs < ActiveRecord::Migration
   def change
     add_column :songs, :artist_id, :integer
     add_column :songs, :genre_id, :integer
-  end
+  end 
 end
